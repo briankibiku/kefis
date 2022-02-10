1f5e2efd09af0ad1f4e46c7149fb74ec6f570d35
kibiku <kibikukimani2@gmail.com>
2022-02-10 09:51:53 +0300

    fix: fix loading not showing

--- nuxt.config.js
+++ nuxt.config.js
@@ -23,6 +23,11 @@ export default {
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: ["~/assets/main.css"],
 
+  loading: {
+    color: 'purple',
+    height: '5px'
+  },
+
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [{ src: "~/plugins/persistedState.client.js" }],
 
