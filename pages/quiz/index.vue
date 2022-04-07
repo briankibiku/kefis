d89265220360a1a9cf923cfb146be06a497d8c78
kibiku <kibikukimani2@gmail.com>
2022-04-07 16:14:39 +0300

    fix: delay move to next question to 4 secs

--- pages/quiz/index.vue
+++ pages/quiz/index.vue
@@ -258,7 +258,7 @@ export default {
         // user selected correct answer
         this.isCorrect = true;
       }
-      await this.$store.dispatch("delayTwoSeconds"),
+      await this.$store.dispatch("delayFourSeconds"),
         await this.goToNextQuestion(answer, selectedchoice);
       this.forceRerender();
       this.resetTimer();
