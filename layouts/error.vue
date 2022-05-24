2640966a3ee22ba995041bdcd633c32447da793c
kibiku <kibikukimani2@gmail.com>
2022-05-24 11:05:38 +0300

    o fix: error page on pwa

---
+++ layouts/error.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="centered-container">
+    <div
+      v-if="
+        error.statusCode === 404 ||
+        error.statusCode === 502 ||
+        error.statusCode === 504
+      "
+    >
+      <img src="~/assets/errorbot.gif" width="260" alt="" />
+      <div class="heading1" style="color: #160d3d">Oops!!</div>
+      <div class="heading4 text-center">
+        An error occurred, we are working on resolving it
+      </div>
+      Go to
+      <nuxt-link class="heading3" style="text-decoration: underline" to="/home"
+        >Home page</nuxt-link
+      >
+    </div>
+    <h1 v-else>An error occurred</h1>
+  </div>
+</template>
+<script>
+export default {
+  props: ["error"],
+};
+</script>
